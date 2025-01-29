d530e3ea33909775e2f7c1f408053fefafe7addf
Mürsel SEN <murselsen_35@hotmail.com>
2025-01-29 23:24:03 +0300

    Enhance console logging for customer methods to improve output clarity

--- js/task-1.js
+++ js/task-1.js
@@ -33,7 +33,7 @@ const customer = {
 };
 
 customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
+console.log('getDiscount: ', customer.getDiscount());
 customer.addOrder(5000, 'Steak');
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+console.log('getBalance: ', customer.getBalance());
+console.log('getOrders: ', customer.getOrders());
