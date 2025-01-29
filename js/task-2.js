ada35f74ddef44424fb12a6a1450d8f274756d30
Mürsel SEN <murselsen_35@hotmail.com>
2025-01-29 23:18:24 +0300

    Update console log messages for clarity in Storage class operations

--- js/task-2.js
+++ js/task-2.js
@@ -20,8 +20,8 @@ class Storage {
 }
 
 const storage = new Storage(['Nanitoids', 'Prolonger', 'Antigravitator']);
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+console.log('Main: ', storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 storage.addItem('Droid');
-console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+console.log('Added: ', storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 storage.removeItem('Prolonger');
-console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+console.log('Removed: ', storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
